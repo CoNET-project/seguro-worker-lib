2dd8eb36e9ec5100cdec708b62617d705814999e
Kloak Information Technologies Inc <info@kloak.app>
2021-08-06 11:33:00 -0700

    Added HelloWorld

--- src/workerBridge/workerBridge.ts
+++ src/workerBridge/workerBridge.ts
@@ -1,5 +1,5 @@
 import SubWorker from './SubWorker'
-
+import type { workerCommand } from './index'
 export default class WorkerBridge {
     public workerBridgeClassReady = false
     public mainWorker
@@ -29,10 +29,27 @@ export default class WorkerBridge {
         const testEnv = process.env.NODE_ENV === 'development'
         const portText = testEnv ? '3001' : window.location.port || '3001'
         const port = parseInt(portText, 10)
-        this.mainWorker = new SubWorker('mainWorker.js', port, (init) => {
+        this.mainWorker = new SubWorker( 'mainWorker.js', port, (init) => {
             this.workerBridgeClassReady = true
             this.seguroInitDataTemp = init
             return this.callback()
         })
+    }
+
+    public helloWorld () {
+        const cmd: workerCommand = {
+            cmd: 'helloWorld'
+        }
+        return new Promise((resolve, reject) =>
+            this.mainWorker.append (cmd, response => {
+                if (!response) {
+                    return reject ( new Error ('Workers have no response!'))
+                }
+                if (response.err) {
+                    return reject ( new Error (response.err))
+                }
+                return resolve ( response )
+            })
+        )
     }
 }
